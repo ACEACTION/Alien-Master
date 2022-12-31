9473571717fec3b1847faaa15c2d03e1f99c2414
Rasoul Sharifi <rasoul.sharifi1988@gmail.com>
2022-12-31 20:31:38 +0330

    delete unity project

--- Alien Master/Assets/Scripts/Enemy/EnemyManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class EnemyManager
-{
-    public static List<Enemy> enemiesList = new List<Enemy>();
-    public static List<GameObject> deadEnemies = new List<GameObject>();
-}
