d65f4efc5bae7b4f8bcd25f016a72ff70776fc82
mohammad firoozi <72497068+mohammadfiroozi1@users.noreply.github.com>
2023-01-17 20:59:09 +0330

    end door + end portal added 0-9 lvl

--- Alien Master/Assets/Scripts/Enemy/EnemyManager.cs
+++ Alien Master/Assets/Scripts/Enemy/EnemyManager.cs
@@ -6,4 +6,8 @@ public static class EnemyManager
 {
     public static List<Enemy> enemiesList = new List<Enemy>();
     public static List<GameObject> deadEnemies = new List<GameObject>();
+    public static bool EnemiesListIsEmpty()
+    {
+        return enemiesList.Count == 0;
+    }
 }
